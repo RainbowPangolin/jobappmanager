c3e75c84fd15f0ca0dfd62f6483d7ba6d44d14a2
Kaiwen Tsou <kktsou@ucsd.edu>
2023-05-31 16:26:40 -0700

    Prototype

--- pages/api/mockdb/db.js
+++ pages/api/mockdb/db.js
@@ -1,6 +1,29 @@
 import fs from 'fs';
 
+function readJSONFile(filename) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filename, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        try {
+          const jsonData = JSON.parse(data);
+          resolve(JSON.stringify(jsonData));
+        } catch (parseError) {
+          reject(parseError);
+        }
+      }
+    });
+  });
+}
+
 export default (req, res) => {
-  // console.log(req.headers)
-  res.status(200).json({ message: 'File operations completed.' });
+
+  readJSONFile('pages/api/mockdb/testData.json')
+  .then((d) => {
+    res.status(200).json({ message: `File operations completed: ${d}` })
+  })
+  .catch ((e => {
+    res.status(200).json({ message: `File operations failed: ${e}` })
+  }))
 };
--- pages/example.js
+++ pages/example.js
@@ -1,12 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Home() {
+
+  const [pee, setPee] = useState('asdf');
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(`${window.location.origin}/api/mockdb/db`);
         const data = await response.json();
+        setTimeout(() => {
+          console.log('wait...')
+          setPee(JSON.stringify(data));
+        }, 1000)
         console.log(data);
+        
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -15,5 +22,7 @@ export default function Home() {
     fetchData();
   }, []);
 
-  return <div>Poo</div>;
+  return (
+    <div>{pee}</div>
+  );
 }
