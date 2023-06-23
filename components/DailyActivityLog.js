6f870f0cd8648e6a165824d4a8c2920feb9a0a5e
Kaiwen Tsou <kktsou@ucsd.edu>
2023-06-23 01:30:59 -0700

    More todo

--- components/DailyActivityLog.js
+++ components/DailyActivityLog.js
@@ -1,8 +1,16 @@
-export default function DailyActivityLog(){
+import * as db from './utils/localDB';
 
+export default function DailyActivityLog(){
+	
 	//Get DAL data
-	//Generate grid based on queried state
-	//Populate grid with data
+	let importedTracker = db.getActivityTracker();
+	console.log(importedTracker);
+	//Generate table based on queried state
+	//generate table of last 60 (variable) days
+	//Filter data to past 60 days
+	
+	//For each entry in the filtered tracker, assign to corresponding table element
+
 
 	return(
 		<div>
